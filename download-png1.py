3088576fa2f8af92ffc86f16b7a58e233806a0d6
ohyujeong <ohlife4@gmail.com>
2022-09-18 14:24:42 +0900

    Ch1 Get 요청

--- download-png1.py
+++ download-png1.py
@@ -1,10 +1,17 @@
-# IP 확인 API로 접근해서 결과 출력하기
-# 모듈 읽어 들이기 --- (※1)
 import urllib.request
-# 데이터 읽어 들이기 --- (※2)
-url = "http://api.aoikujira.com/ip/ini"
-res = urllib.request.urlopen(url)
-data = res.read()
-# 바이너리를 문자열로 변환하기 --- (※3)
+import urllib.parse
+
+api = "https://search.naver.com/search.naver"
+values = {
+    "where":"nexearch",
+    "sm":"top_hty",
+    "fbm":"1",
+    "ie":"utf8",
+    "query":"초콜릿"
+}
+parmas = urllib.parse.urlencode(values)
+url = api + "?" + parmas
+
+data = urllib.request.urlopen(url).read()
 text = data.decode("utf-8")
 print(text)
