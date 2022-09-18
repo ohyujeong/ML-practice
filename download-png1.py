1eeb6b68dee65913be515f24cf3572b146eae4df
ohyujeong <ohlife4@gmail.com>
2022-09-18 14:11:18 +0900

    Ch1 decode

--- download-png1.py
+++ download-png1.py
@@ -1,13 +1,10 @@
+# IP 확인 API로 접근해서 결과 출력하기
+# 모듈 읽어 들이기 --- (※1)
 import urllib.request
-
-url = "http://uta.pw/shodou/img/28/214.png"
-savename = "test.png"
-
-# urllib.request.urlretrieve(url, savename)
-
-# 다운로드
-mem = urllib.request.urlopen(url).read()
-
-with open(savename, mode="wb") as f:
-    f.write(mem)
-    print("저장되었습니다!")
+# 데이터 읽어 들이기 --- (※2)
+url = "http://api.aoikujira.com/ip/ini"
+res = urllib.request.urlopen(url)
+data = res.read()
+# 바이너리를 문자열로 변환하기 --- (※3)
+text = data.decode("utf-8")
+print(text)
