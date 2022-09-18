fd6f24f8d997a5d9b111cfbaff15212063bbbf4e
ohyujeong <ohlife4@gmail.com>
2022-09-18 14:02:11 +0900

    Ch1 이미지 다운로드

---
+++ download-png1.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+url = "http://uta.pw/shodou/img/28/214.png"
+savename = "test.png"
+
+# urllib.request.urlretrieve(url, savename)
+
+# 다운로드
+mem = urllib.request.urlopen(url).read()
+
+with open(savename, mode="wb") as f:
+    f.write(mem)
+    print("저장되었습니다!")
