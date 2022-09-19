686d078f37b3b2bf1874cc89ebcfff37e8498e8e
ohyujeong <ohlife4@gmail.com>
2022-09-19 23:09:18 +0900

    ch5 다음기사로 연습

--- bs-test2.py
+++ bs-test2.py
@@ -3,23 +3,25 @@ from bs4 import BeautifulSoup
 
 import time
 
-url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105"
+url = "https://news.daum.net/digital#1"
 response = urllib.request.urlopen(url)
 
 soup = BeautifulSoup(response, "html.parser")
-results = soup.select("#section_body a")
+results = soup.select("ul.list_newsmajor li strong a.link_txt")
 
-# results = soup.select("span.cluster_head_sub_topic")
-
+# for result in results:
+#     print(result.string)
 for result in results:
     # print(result.string)
-    print(result.attrs["title"])
+    print(result.string)
+    # print(result.attrs["href"])
+    # print("")
     url_article = result.attrs["href"]
     response = urllib.request.urlopen(url_article)
     soup_article = BeautifulSoup(response, "html.parser")
-    content = soup_article.select_one("articleBodyContents")
+    content = soup_article.find('p', attrs={'dmcf-ptype':'general'})
     
-    #string 안 되면 contents
+    # #string 안 되면 contents
     print(content.contents)
-    #1초 휴식
+    # #1초 휴식
     time.sleep(1)
